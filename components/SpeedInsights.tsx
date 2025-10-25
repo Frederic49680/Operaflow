5f96993ec0da877e9c76db62eea57f1bc27c4f61
Fred Baudry <fredericbaudry49680@gmail.com>
2025-10-25 13:33:06 +0200

    Temporarily disable Speed Insights to fix deployment

--- components/SpeedInsights.tsx
+++ components/SpeedInsights.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 export default function SpeedInsightsComponent() {
   return <SpeedInsights />
